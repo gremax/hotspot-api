251c808684ed10c53c34c3a700fc179d4899f575
Max Grechko <gremax@gremax.me>
2018-02-23 21:59:32 +0200

    Add json web token library to encode/decode tokens

---
+++ spec/lib/json_web_token_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+require 'json_web_token'
+
+RSpec.describe JsonWebToken do
+  let(:secret_key) { Rails.application.credentials.config[:secret_key_base] }
+
+  describe '.encode' do
+    let(:payload) { {} }
+
+    it 'calls JWT.encode with payload and secret' do
+      expect(JWT)
+        .to receive(:encode)
+        .with(payload, secret_key)
+
+      described_class.encode(payload)
+    end
+
+    it 'returns the token' do
+      allow(JWT)
+        .to receive(:encode)
+        .with(payload, secret_key)
+        .and_return('jwt-token')
+
+      expect(described_class.encode(payload)).to eq('jwt-token')
+    end
+  end
+
+  describe '.decode' do
+    let(:token) { 'jwt-token' }
+
+    it 'calls JWT.decode with token and secret' do
+      expect(JWT)
+        .to receive(:decode)
+        .with(token, secret_key)
+
+      described_class.decode(token)
+    end
+
+    it 'returns the decoded body' do
+      allow(JWT)
+        .to receive(:decode)
+        .with(token, secret_key)
+        .and_return([{ foo: 'bar' }])
+
+      expect(described_class.decode(token)).to eq('foo' => 'bar')
+    end
+  end
+end
