04aca097f601bad4e066da4a2066002b0bf093b8
Max Grechko <gremax@gremax.me>
2018-02-25 17:16:03 +0200

    Configure warden-based authentication

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,12 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include Warden::Test::Helpers
+
+  config.after(:each) do
+    Warden.test_reset!
+  end
 end
