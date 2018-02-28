60cd132eed41492b4ffbecaf4bca90a674f61b8d
Max Grechko <gremax@gremax.me>
2018-02-28 20:46:34 +0200

    Add custom JSON Api errors formatter

---
+++ app/services/exceptions/form_errors.rb
@@ -0,0 +1,21 @@
+module Exceptions
+  class FormErrors < ::JSONAPI::Exceptions::Error
+    attr_reader :object
+
+    def initialize(object)
+      @object = object
+    end
+
+    def errors
+      object.errors.messages.map do |attribute, errors|
+        JSONAPI::Error.new(
+          id: attribute,
+          title: errors.first,
+          detail: "#{attribute.capitalize} #{errors.first}",
+          status: :unprocessable_entity,
+          source: { pointer: "/data/attributes/#{attribute}" }
+        )
+      end
+    end
+  end
+end
