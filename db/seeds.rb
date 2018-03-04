f2bea419f6035b062f66396edc0412586a86c97c
Max Grechko <gremax@gremax.me>
2018-03-04 13:42:49 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+require 'factory_bot'
+
+account = Account.find_or_create_by(subdomain: 'hotspot', name: 'Hotspot Inc.')
+
+unless Apartment.tenant_names.include?(account.subdomain)
+  CreateTenant.new(account).call
+end
+
+Apartment::Tenant.switch(account.subdomain) do |_tenant|
+  admin = AccountAdmin.find_or_create_by(email: 'admin@hotspot.dev',
+                                         first_name: 'Admin',
+                                         last_name: 'Hotspot')
+  admin.update(password: 'password')
+end
