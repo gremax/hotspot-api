e5584c4125a3a73da1b773b51081184d9d526ca3
Max Grechko <gremax@gremax.me>
2018-04-22 17:32:39 +0300

    Add UpdateAdmin transaction

---
+++ app/transactions/update_admin.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class UpdateAdmin
+  include Dry::Transaction
+
+  step :assign_attributes
+  step :validate
+  step :persist
+  step :display
+
+  def assign_attributes(input)
+    admin = Admin.find(input[:id])
+    admin.assign_attributes(input[:params])
+    Success(admin)
+  end
+
+  def validate(entity)
+    form = AdminForm.new(entity)
+
+    if form.valid?
+      Success(form)
+    else
+      Failure(form)
+    end
+  end
+
+  def persist(form)
+    form.save
+    Success(form)
+  end
+
+  def display(form)
+    Success(form.model)
+  end
+end
