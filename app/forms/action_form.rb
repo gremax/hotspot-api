ffdb7e6f0a605ef7d864d8b6b23356d62c9bc873
Max Grechko <gremax@gremax.me>
2018-05-03 19:56:47 +0300

    Add ActionForm

---
+++ app/forms/action_form.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class ActionForm < Reform::Form
+  property :provider_id
+  property :current
+  property :name
+  property :entity_uid
+  property :entity_name
+  property :post_text
+  property :post_link
+  property :post_image
+  property :redirect_url
+
+  validation do
+    configure do
+      option :provider, Provider
+
+      def record?(klass, value)
+        klass.where(id: value).any?
+      end
+    end
+
+    required(:provider_id).filled(record?: Provider)
+    required(:name).filled
+
+    validate(eligible_name: %i[provider_id name]) do |provider_id, name|
+      provider = Provider.find(provider_id)
+      return false if provider.nil?
+      Provider::ELIGIBLE_ACTIONS[provider.name].include?(name)
+    end
+  end
+end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -3,6 +3,12 @@
 class Provider < ApplicationRecord
   AVAILABLE_NAMES = %w[facebook twitter instagram].freeze
 
+  ELIGIBLE_ACTIONS = {
+    'facebook' => %w[post join like],
+    'twitter' => %w[tweet retweet follow like],
+    'instagram' => %w[post follow like]
+  }.freeze
+
   belongs_to :company
   has_many :actions, dependent: :destroy
 
