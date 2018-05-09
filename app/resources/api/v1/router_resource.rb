b1a020fbbdaca8446b3c940d7458b9fef0eebf0a
Max Grechko <gremax@gremax.me>
2018-05-09 19:45:10 +0300

    Add ProviderResource

--- app/resources/api/v1/router_resource.rb
+++ app/resources/api/v1/router_resource.rb
@@ -3,7 +3,8 @@
 module Api
   module V1
     class RouterResource < JSONAPI::Resource
-      attributes :name, :username, :place_id, :status, :last_pinged_at, :created_at, :updated_at
+      attributes :name, :username, :place_id, :status, :last_pinged_at,
+                 :created_at, :updated_at
 
       has_one :place
     end
